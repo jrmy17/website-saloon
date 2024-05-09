b571a0813fda2c077344aa5756796da03bc29641
Jérémy PION <Jeremy.pionsfr@gmail.com>
2024-05-09 22:50:28 +0200

    add border

--- app/page.tsx
+++ app/page.tsx
@@ -24,8 +24,8 @@ export default function Home() {
   return (
     <main className="">
       <nav
-        className={`flex items-center justify-between p-4 pl-80 pr-80 text-white top-0 z-50 transition-colors fixed left-0 right-0 ${
-          isScrolled ? "bg-white text-black p-2" : "bg-transparent"
+        className={`flex items-center justify-between p-4 pl-80 pr-80 top-0 z-50 transition-colors fixed left-0 right-0 ${
+          isScrolled ? "bg-white text-black p-2" : "bg-transparent text-white"
         }`}
         style={{
           transition: "background-color 0.5s, padding 0.5s",
@@ -66,12 +66,12 @@ export default function Home() {
         style={{ backgroundImage: "url('/background.jpg')" }}
       >
         <div
-          className="absolute top-0 left-0 right-0 bottom-0 flex items-center justify-center text-white text-6xl font-bold flex-col"
+          className="absolute top-0 left-0 right-0 bottom-0 flex items-center justify-center text-white text-8xl font-bold flex-col"
           style={{
             background: "rgba(0, 0, 0, 0.7)",
           }}
         >
-          <h2 className="text-2xl font-bold text-white">
+          <h2 className="text-2xl text-white font-light">
             Serveur RedM FreeAccess
           </h2>
           <h1>Saloon RôlePlay</h1>
@@ -79,7 +79,7 @@ export default function Home() {
         <div>
           <svg
             viewBox="0 0 1500 200"
-            className="w-full absolute bottom-[-28px]"
+            className="w-full absolute bottom-[-35px]"
           >
             <path
               className="fill-white"
@@ -89,12 +89,13 @@ export default function Home() {
         </div>
       </header>
       <section className="p-6 pl-[30rem] pr-[30rem]">
-        <h2 className="text-2xl font-bold text-black text-center">
+        <h2 className="text-4xl font-bold text-black text-center">
           Notre serveur
         </h2>
         <div className="flex justify-center space-x-4 mt-8">
-          <div className="w-1/3">
-            <h3 className="text-xl font-bold text-black">01</h3>
+          <div className="w-1/3 border-solid border-2 border-gray-200 p-4">
+            <h3 className="text-l font-bold text-red-500">01</h3>
+            <h4 className="text-2xl font-bold text-black">Lorem ipsum</h4>
             <p className="text-black">
               Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc
               tincidunt, nunc nec consectetur. Lorem ipsum dolor sit amet,
@@ -104,8 +105,9 @@ export default function Home() {
               consectetur adipiscing elit. Nunc tincidunt, nunc nec consectetur.
             </p>
           </div>
-          <div className="w-1/3">
-            <h3 className="text-xl font-bold text-black">02</h3>
+          <div className="w-1/3 border-solid border-2 border-gray-200 p-4">
+            <h3 className="text-l font-bold text-red-500">02</h3>
+            <h4 className="text-2xl font-bold text-black">Lorem ipsum</h4>
             <p className="text-black">
               Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc
               tincidunt, nunc nec consectetur. Lorem ipsum dolor sit amet,
@@ -115,8 +117,9 @@ export default function Home() {
               consectetur adipiscing elit. Nunc tincidunt, nunc nec consectetur.
             </p>
           </div>
-          <div className="w-1/3">
-            <h3 className="text-xl font-bold text-black">03</h3>
+          <div className="w-1/3 border-solid border-2 border-gray-200 p-4">
+            <h3 className="text-l font-bold text-red-500">03</h3>
+            <h4 className="text-2xl font-bold text-black">Lorem ipsum</h4>
             <p className="text-black">
               Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc
               tincidunt, nunc nec consectetur. Lorem ipsum dolor sit amet,
@@ -129,7 +132,7 @@ export default function Home() {
         </div>
       </section>
       <section className="p-6 pl-[30rem] pr-[30rem] bg-slate-100">
-        <h2 className="text-2xl font-bold text-black text-left">
+        <h2 className="text-4xl font-bold text-black text-left">
           A propos de nous
         </h2>
         <div className="flex justify-center space-x-4 mt-8">
